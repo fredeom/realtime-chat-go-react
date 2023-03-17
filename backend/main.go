eda34151c2b2b65605e03b035ce8e59ec0d97df5
Alexander Smirnov <fredeom@mail.ru>
2023-03-17 09:24:21 +0300

    Clean up main.go

--- backend/main.go
+++ backend/main.go
@@ -7,31 +7,6 @@ import (
 	"github.com/freadeom/realtime-chat-go-react/pkg/websocket"
 )
 
-/*
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
-
-func reader(conn *websocket.Conn) {
-	for {
-		messageType, p, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
-	}
-}
-*/
-
 func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
 	fmt.Println("WebSocket Endpoint Hit")
 	conn, err := websocket.Upgrade(w, r)
@@ -45,8 +20,6 @@ func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
 
 	pool.Register <- client
 	client.Read()
-	//go websocket.Writer(ws)
-	//websocket.Reader(ws)
 }
 
 func setupRoutes() {
@@ -63,6 +36,3 @@ func main() {
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
 }
-
-//docker build -t backend .
-//docker run -it -p 8080:8080 backend
